48fd8491fc763b678ac30ea47b3c76d4b8b63028
Carrie Mathieu <carrieburks@gmail.com>
2020-08-06 14:01:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,7 @@
 def position_taken?(board, index)
   if board[index] == "" || board[index] == " " || board[index] == nil
     FALSE
+  elsif board[index] == "X" || board[index] == "O"
+    true
   end
 end
